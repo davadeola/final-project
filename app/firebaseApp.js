d15b717d094eb9c0d8f51d418c19e48d2300f31d
davadeola <adeoladavid15@gmail.com>
2021-11-06 13:30:28 +0300

    Added login

--- app/firebaseApp.js
+++ app/firebaseApp.js
@@ -1,13 +1,11 @@
 import { initializeApp, getApps } from "firebase/app";
 import firebase from "firebase/compat/app";
-import { getAuth } from "firebase/auth";
-import { firebaseConfig } from "../config/firebaseApp.config.js";
-import { GoogleAuthProvider } from "firebase/auth";
 
-const provider = new GoogleAuthProvider();
+import { firebaseConfig } from "../config/firebaseApp.config.js";
 
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-export { firebase, auth };
+const provider = new GoogleAuthProvider();
+export { firebase, auth, provide };
--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,32 @@
 // Firebase related
 
-import { auth } from "../app/firebaseApp";
+import { useState } from "react";
+import { auth, provider } from "../app/firebaseApp";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
 import styles from "../styles/Home.module.css";
 
 export default function Login() {
+  const loginWithFirebase = () => {
+    const auth = getAuth();
+
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+
+        const user = result.user;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        const email = error.email;
+
+        const credential = GoogleAuthProvider.credentialFromError(error);
+      });
+  };
+
   return (
     <div className={styles.container}>
       <div className="container" style={{ width: "100vw" }}>
@@ -20,7 +43,10 @@ export default function Login() {
             }}
           >
             <h2>Login into your Account</h2>
-            <button class="btn btn-primary">Login with Google</button>
+            <p className="lead">Step into your account to start.</p>
+            <button className="btn btn-primary" onClick={loginWithFirebase}>
+              Login with Google
+            </button>
           </div>
         </div>
       </div>
